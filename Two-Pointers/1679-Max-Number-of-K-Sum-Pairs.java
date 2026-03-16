b89fdda4eb19b1e943d7be0a8037ae88316f8c6a
Srutirani Sahoo <srutiranisahoo2@gmail.com>
2026-03-16 10:43:22 +0530

    Create 1679-Max-Number-of-K-Sum-Pairs.java

---
+++ Two-Pointers/1679-Max-Number-of-K-Sum-Pairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int cnt=0;
+        int i=0,j=nums.length-1;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                cnt++;
+                i++;
+                j--;
+            }
+            else if(nums[i]+nums[j]>k){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return cnt;
+    }
+}
